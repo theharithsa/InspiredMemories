4a3fc4201063353e48d4f60f4c7abec1dc91703d
Vishruth Harithsa BN <harithsa@outlook.com>
2016-02-10 22:09:30 +0530

    Whole Bunch of Chages

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate(){
-var username = document.getElementById("login-username").value;
-var password = document.getElementById("login-password").value;
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
 if ( username == "Spoorthi" && password == "Paintings"){
 alert ("Login successfully");
 window.location = "Dashboard/dashboard.html"; // Redirecting to other page.
@@ -13,7 +13,7 @@ attempt --;// Decrementing by one.
 alert("You have left "+attempt+" attempt;");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
-document.getElementById("username").disabled = true;
+    document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
 return false;
