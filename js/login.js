fac2ce26d2f5200cfbd8ed7cf977e9fd00bd1992
Vishruth Harithsa BN <harithsa@outlook.com>
2016-02-11 21:26:21 +0530

    Login and Favicon Changes

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "Spoorthi" && password == "Paintings"){
-alert ("Login successfully");
+alert ("Login successfull");
 window.location = "Dashboard/dashboard.html"; // Redirecting to other page.
 return false;
 }
@@ -19,4 +19,33 @@ document.getElementById("submit").disabled = true;
 return false;
 }
 }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        alert("Please Login to Continue", "");
+        window.location = "../index.html";
+    }
+
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
 }
